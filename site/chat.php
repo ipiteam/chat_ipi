1170ac1dd1678d70a04cd790ee8a903135cef1b9
Denis Ma <madenis86@gmail.com>
2016-12-13 22:19:30 +0100

    Renommage html en php + rendu modulable.

---
+++ site/chat.php
@@ -0,0 +1,17 @@
+<?php
+    include('header.php');
+?>
+    <!-- Page Content -->
+    <div class="container">
+        <div class="ipi_membres">
+            
+        </div>
+        <div class="ipi_mainchat">
+            
+
+        </div>
+
+    <!-- /.container -->
+<?php
+    include('footer.php');
+?>
---
+++ site/nav.php
@@ -0,0 +1,43 @@
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">
+                    <img src="img/title.jpg" alt="">
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="chat.php">Chat</a>
+                    </li>
+                    <li>
+                        <a href="profil.php">Profil</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <form class="navbar-form">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control_head" placeholder="Recherche">
+                            </div>
+                            <button type="submit" class="btn btn-default btn-default_head"><span class="glyphicon glyphicon-search"></span></button>
+                        </form>
+                    </li>
+                    <li>
+                        <a href="index.php">Déconnexion</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
