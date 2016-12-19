bfe93e8d14bfa1b4e50cb2f03d2312b3db5d83cd
Mo The Black Cat <motheblackcat@free.fr>
2016-12-19 14:15:03 +0100

    Retrait de la liste des personnes connectées.

--- site/chat.php
+++ site/chat.php
@@ -7,10 +7,10 @@ include('nav.php');
     <!-- Page Content -->
     <div class="container">
 		<div class="row">
-			<div class="ipi_membres col-lg-2 col-lg-offset-2 col-md-2 col-md-offset-2">
+			<!--<div class="ipi_membres col-lg-2 col-lg-offset-2 col-md-2 col-md-offset-2">
 				<p>Liste des membres</p>
-			</div>
-			<div id="ipi_mainchat" class="col-lg-6 col-md-6">
+			</div>-->
+			<div id="ipi_mainchat" class="col-lg-6 col-md-6 col-lg-offset-3 col-md-offset-3">
 				<div id="chat">
 					<div class="msg_get">
 						<div class="oheure">17:16:25</div>
@@ -96,7 +96,7 @@ include('nav.php');
 			</div>
 		</div>
 		<div class="row">
-				<div id="envoieMsg" class="col-lg-6 col-lg-offset-4 col-md-6 col-md-offset-4 col-sm-12 col-xs-12" >
+				<div id="envoieMsg" class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-12 col-xs-12" >
 					<textarea id="sendTextArea" class="col-lg-10 col-md-10 col-sm-10 col-xs-10" name=""></textarea>
 					<input type="button" id="sendButton" value="envoyer" class="btn-primary col-lg-2 col-md-2 col-sm-2 col-xs-2">
 				</div>
